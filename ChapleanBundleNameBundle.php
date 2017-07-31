5b09f6f54b43d20dfcb822468d029fa9e9ff1c38
Tom - Chaplean <tom@chaplean.coop>
2017-07-31 14:10:12 +0200

    Upgrading auth key, composer.json and chaplean.com to chaplean.coop

--- ChapleanBundleNameBundle.php
+++ ChapleanBundleNameBundle.php
@@ -8,8 +8,8 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  * Class ChapleanBundleNameBundle.
  *
  * @package   Chaplean\Bundle\BundleNameBundle
- * @author    Valentin - Chaplean <valentin@chaplean.com>
- * @copyright 2014 - 2015 Chaplean (http://www.chaplean.com)
+ * @author    Valentin - Chaplean <valentin@chaplean.coop>
+ * @copyright 2014 - 2015 Chaplean (http://www.chaplean.coop)
  * @since     1.0.0
  */
 class ChapleanBundleNameBundle extends Bundle
